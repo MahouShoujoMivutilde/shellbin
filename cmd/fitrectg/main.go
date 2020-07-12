6be198ed8638f1bc1f3ac31214855e9ee6a35589
MahouShoujoMivutilde <mivu.sedai@gmail.com>
2020-07-12 08:53:42 +0300

    no more underscores in variables

--- cmd/fitrectg/main.go
+++ cmd/fitrectg/main.go
@@ -11,9 +11,9 @@ func usage() {
 	fmt.Printf("%[1]s - fit image into given rectangle while preserving aspect ratio\nOutputs new width and height as WxH.", os.Args[0])
 }
 
-func fitIntoRect(W float64, H float64, rect_W float64, rect_H float64) (int, int) {
-	s1 := rect_W / W
-	s2 := rect_H / H
+func fitIntoRect(W float64, H float64, rectW float64, rectH float64) (int, int) {
+	s1 := rectW / W
+	s2 := rectH / H
 	if s1 < s2 {
 		W = W * s1
 		H = H * s1
@@ -29,16 +29,16 @@ func main() {
 	flag.Usage = usage
 	W := flag.Float64("w", 0, "current image width")
 	H := flag.Float64("h", 0, "current image height")
-	rect_W := flag.Float64("fw", 0, "rectangle width")
-	rect_H := flag.Float64("fh", 0, "rectangle height")
+	rectW := flag.Float64("fw", 0, "rectangle width")
+	rectH := flag.Float64("fh", 0, "rectangle height")
 
 	flag.Parse()
 
-	if *W == 0 || *H == 0 || *rect_H == 0 || *rect_W == 0 {
+	if *W == 0 || *H == 0 || *rectH == 0 || *rectW == 0 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	out_W, out_H := fitIntoRect(*W, *H, *rect_W, *rect_H)
-	fmt.Printf("%dx%d\n", out_W, out_H)
+	outW, outH := fitIntoRect(*W, *H, *rectW, *rectH)
+	fmt.Printf("%dx%d\n", outW, outH)
 }
