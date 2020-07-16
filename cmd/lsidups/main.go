7f01ba14aab50aa882cf38188514f205c93c8d97
MahouShoujoMivutilde <mivu.sedai@gmail.com>
2020-07-16 19:14:44 +0300

    lsidups - split reusable stuff into internal/

--- cmd/lsidups/main.go
+++ cmd/lsidups/main.go
@@ -4,14 +4,40 @@ import (
 	"flag"
 	"fmt"
 	"image"
-	"os"
-	"path/filepath"
 	"runtime"
 	"strings"
 	"sync"
+	"time"
 
+	"github.com/MahouShoujoMivutilde/shellbin/internal/filehelpers"
+	"github.com/MahouShoujoMivutilde/shellbin/internal/in"
 	"github.com/vitali-fedulov/images"
 )
+
+type extensions []string
+
+func (e *extensions) String() string {
+	return strings.Join(*e, ",")
+}
+
+func (e *extensions) Set(val string) error {
+	*e = extensions{}
+	for _, ext := range strings.Split(val, ",") {
+		*e = append(*e, ext)
+	}
+	return nil
+}
+
+var searchExt extensions
+var input string
+var verbose bool
+
+func init() {
+	searchExt = extensions{".jpg", ".jpeg", ".png", ".gif"}
+	flag.Var(&searchExt, "e", "image extensions (with dots) to look for")
+	flag.StringVar(&input, "p", ".", "find duplicate images in given directory")
+	flag.BoolVar(&verbose, "v", false, "show time it took to complete key parts of the search")
+}
 
 type Image struct {
 	fp      string
@@ -19,54 +45,7 @@ type Image struct {
 	imgSize image.Point
 }
 
-// checks if path has hidden elements, unix only
-func isHidden(fp string) bool {
-	for _, element := range strings.Split(fp, string(filepath.Separator)) {
-		if strings.HasPrefix(element, ".") && element != "." {
-			return true
-		}
-	}
-	return false
-}
-
-func getFiles(dir string) ([]string, error) {
-	var files []string
-	err := filepath.Walk(dir, func(fp string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			abs, err := filepath.Abs(fp)
-			if err == nil {
-				files = append(files, abs)
-			}
-		}
-		return nil
-	})
-	return files, err
-}
-
-func filterFiles(slice []string, condition func(string) bool) []string {
-	var newSlice []string
-	for _, element := range slice {
-		if condition(element) {
-			newSlice = append(newSlice, element)
-		}
-	}
-	return newSlice
-}
-
-// takes file pathes list and list of extensions, retuns pathes with thouse extensions
-func filterExt(files []string, searchExt []string) []string {
-	return filterFiles(files, func(fp string) bool {
-		for _, ext := range searchExt {
-			fpext := strings.ToLower(filepath.Ext(fp))
-			if strings.Contains(fpext, ext) {
-				return true
-			}
-		}
-		return false
-	})
-}
-
-func hashImage(fp string) Image {
+func makeImage(fp string) Image {
 	pic, err := images.Open(fp)
 	if err != nil {
 		panic(err)
@@ -75,38 +54,30 @@ func hashImage(fp string) Image {
 	return Image{fp, imgHash, imgSize}
 }
 
-func worker(jobs <-chan string, results chan<- Image, wg *sync.WaitGroup) {
+func imageMaker(jobs <-chan string, results chan<- Image, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for fp := range jobs {
-		results <- hashImage(fp)
+		results <- makeImage(fp)
 	}
-}
-
-// Contains tells whether a contains x.
-func Contains(a []string, x string) bool {
-	for _, n := range a {
-		if x == n {
-			return true
-		}
-	}
-	return false
 }
 
 func main() {
-	// start := time.Now()
-
-	searchExt := []string{".jpg", ".jpeg", ".png", ".gif"}
-	p := flag.String("p", ".", "find duplicate images in given directory")
 	flag.Parse()
 
-	files, _ := getFiles(*p)
-	files = filterFiles(files, func(fp string) bool { return !isHidden(fp) })
+	start := time.Now()
 
-	files = filterExt(files, searchExt)
+	files, _ := filehelpers.GetFiles(input)
+	files = filehelpers.FilterFiles(files, func(fp string) bool {
+		return !filehelpers.IsHidden(fp)
+	})
 
-	// fmt.Println("got files", time.Since(start))
+	files = filehelpers.FilterExt(files, searchExt)
 
-	// start = time.Now()
+	if verbose {
+		fmt.Printf("> found %d images, took %s\n", len(files), time.Since(start))
+	}
+
+	start = time.Now()
 
 	numJobs := len(files)
 	jobs := make(chan string, numJobs)
@@ -115,7 +86,7 @@ func main() {
 	var wg sync.WaitGroup
 	for w := 1; w <= runtime.NumCPU(); w++ {
 		wg.Add(1)
-		go worker(jobs, results, &wg)
+		go imageMaker(jobs, results, &wg)
 	}
 
 	for _, fp := range files {
@@ -127,21 +98,25 @@ func main() {
 	// yay, antipatterns!
 	close(results)
 
+	if verbose {
+		fmt.Printf("> processed image parameters, took %s\n", time.Since(start))
+	}
+
 	var pics []Image
 	for pic := range results {
 		pics = append(pics, pic)
 	}
 
-	// start := time.Now()
+	start = time.Now()
 	var dups []string
 	for _, pic := range pics {
 		for _, ipic := range pics {
 			if ipic.fp != pic.fp {
 				if images.Similar(ipic.imgHash, pic.imgHash, ipic.imgSize, pic.imgSize) {
-					if !Contains(dups, pic.fp) {
+					if !in.ContainsStr(dups, pic.fp) {
 						dups = append(dups, pic.fp)
 					}
-					if !Contains(dups, ipic.fp) {
+					if !in.ContainsStr(dups, ipic.fp) {
 						dups = append(dups, ipic.fp)
 					}
 				}
@@ -149,8 +124,11 @@ func main() {
 		}
 	}
 
-	// fmt.Println("search took", time.Since(start))
 	for _, fp := range dups {
 		fmt.Println(fp)
+	}
+
+	if verbose {
+		fmt.Printf("> search of similar images took %s\n", time.Since(start))
 	}
 }
---
+++ internal/in/in.go
@@ -0,0 +1,22 @@
+// Package in implements pythonic "in" for few basic types
+package in
+
+// ContainsStr tells whether slice contains x.
+func ContainsStr(slice []string, x string) bool {
+	for _, n := range slice {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+// ContainsInt tells whether slice contains x.
+func ContainsInt(slice []int, x int) bool {
+	for _, n := range slice {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
