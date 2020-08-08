ec3ad2e11cfcec214301246f9ee0d3ccf0a553e6
MahouShoujoMivutilde <mivu.sedai@gmail.com>
2020-08-08 19:50:51 +0300

    aqua - compare 2 images using goimagehash

---
+++ cmd/aqua/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"os"
+
+	"github.com/corona10/goimagehash"
+)
+
+func main() {
+	var fp1, fp2 string
+	flag.StringVar(&fp1, "a", "", "file a")
+	flag.StringVar(&fp2, "b", "", "file b")
+
+	flag.Parse()
+
+	if fp1 == "" || fp2 == "" {
+		os.Exit(1)
+	}
+
+	file1, err := os.Open(fp1)
+	if err != nil {
+		panic(err)
+	} else {
+		defer file1.Close()
+	}
+
+	file2, err := os.Open(fp2)
+	if err != nil {
+		panic(err)
+	} else {
+		defer file1.Close()
+	}
+
+	img1, _, err := image.Decode(file1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	img2, _, err := image.Decode(file2)
+
+	if err != nil {
+		panic(err)
+	}
+
+	hash1, _ := goimagehash.AverageHash(img1)
+	hash2, _ := goimagehash.AverageHash(img2)
+	distance, _ := hash1.Distance(hash2)
+
+	fmt.Printf("average: %v\n", distance)
+
+	hash1, _ = goimagehash.DifferenceHash(img1)
+	hash2, _ = goimagehash.DifferenceHash(img2)
+	distance, _ = hash1.Distance(hash2)
+
+	fmt.Printf("diff: %v\n", distance)
+
+	hash1, _ = goimagehash.PerceptionHash(img1)
+	hash2, _ = goimagehash.PerceptionHash(img2)
+	distance, _ = hash1.Distance(hash2)
+
+	fmt.Printf("phash: %v\n", distance)
+
+	width, height := 16, 16
+	hash3, _ := goimagehash.ExtPerceptionHash(img1, width, height)
+	hash4, _ := goimagehash.ExtPerceptionHash(img2, width, height)
+	distance, _ = hash3.Distance(hash4)
+
+	fmt.Printf("phash big: %v\n", distance)
+	fmt.Printf(" a size: %v\n", hash3.Bits())
+	fmt.Printf(" b size: %v\n", hash4.Bits())
+}
