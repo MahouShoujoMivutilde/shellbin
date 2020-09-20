3235450a8eb81f6a53d87bf709c97e36996d8513
MahouShoujoMivutilde <mivu.sedai@gmail.com>
2020-09-20 16:19:25 +0300

    hum - humanize (time for now)

---
+++ cmd/hum/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/dustin/go-humanize"
+)
+
+var DESC string = os.Args[0] + `
+
+  Is a tool for humanizing various thigs (now supports time)
+
+  echo thing | hum -args...
+
+`
+
+func usage() {
+	fmt.Fprint(flag.CommandLine.Output(), DESC)
+
+	fmt.Fprintf(flag.CommandLine.Output(), "Usage of %[1]s:\n", os.Args[0])
+
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Usage = usage
+
+	timeFormat := flag.String("t",
+		"2006-01-02 15:04:05.999999999 -0700",
+		// defaults to stat human readable format
+		"time format, fill with Mon Jan 2 15:04:05 MST 2006,"+
+			" see https://golang.org/src/time/format.go")
+
+	flag.Parse()
+
+	mode := flag.Arg(0)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		str := strings.TrimSpace(scanner.Text())
+
+		switch mode {
+		case "time":
+			t, err := time.Parse(*timeFormat, str)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(humanize.Time(t))
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/corona10/goimagehash v1.0.2
+	github.com/dustin/go-humanize v1.0.0
 	github.com/gabriel-vasile/mimetype v1.1.1
 	github.com/kortschak/zalgo v0.0.0-20190131100928-344d6584eb92
 	github.com/vitali-fedulov/images v0.0.0-20200831232703-8a90db96a358
